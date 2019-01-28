8f8cfa9bd715e2080f8984e8a282812fb6445702
Ken Chen <kenchen@microsoft.com>
2019-01-28 16:17:23 +0800

    Demo for sharding

--- samples/ChatRoom/Startup.cs
+++ samples/ChatRoom/Startup.cs
@@ -1,12 +1,56 @@
 // Copyright (c) Microsoft. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.Azure.SignalR.Samples.ChatRoom
 {
+    class CustomRouter : IEndpointRouter
+    {
+        DefaultEndpointRouter _inner = new DefaultEndpointRouter();
+
+        public IEnumerable<ServiceEndpoint> GetEndpointsForBroadcast(IEnumerable<ServiceEndpoint> availableEndpoints)
+        {
+            return availableEndpoints;
+        }
+
+        public IEnumerable<ServiceEndpoint> GetEndpointsForConnection(string connectionId, IEnumerable<ServiceEndpoint> availableEndpoints)
+        {
+            return availableEndpoints;
+        }
+
+        public IEnumerable<ServiceEndpoint> GetEndpointsForGroup(string groupName, IEnumerable<ServiceEndpoint> availableEndpoints)
+        {
+            return availableEndpoints;
+        }
+
+        public IEnumerable<ServiceEndpoint> GetEndpointsForGroups(IReadOnlyList<string> groupList, IEnumerable<ServiceEndpoint> availableEndpoints)
+        {
+            return availableEndpoints;
+        }
+
+        public IEnumerable<ServiceEndpoint> GetEndpointsForUser(string userId, IEnumerable<ServiceEndpoint> availableEndpoints)
+        {
+            return availableEndpoints;
+        }
+
+        public IEnumerable<ServiceEndpoint> GetEndpointsForUsers(IReadOnlyList<string> userList, IEnumerable<ServiceEndpoint> availableEndpoints)
+        {
+            return availableEndpoints;
+        }
+
+        public ServiceEndpoint GetNegotiateEndpoint(IEnumerable<ServiceEndpoint> primaryEndpoints)
+        {
+            var e = _inner.GetNegotiateEndpoint(primaryEndpoints);
+            Console.WriteLine($"Available endpoints: {String.Join(',', primaryEndpoints)}, return: {e}");
+            return e;
+        }
+    }
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -18,6 +62,7 @@ namespace Microsoft.Azure.SignalR.Samples.ChatRoom
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(typeof(IEndpointRouter), new CustomRouter());
             services.AddMvc();
             services.AddSignalR()
                     .AddAzureSignalR();
---
+++ samples/ConnectTool/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace signalrtest
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = new HubConnectionBuilder().WithUrl("http://localhost:5000/chat");
+            var conn = builder.Build();
+            while (true)
+            {
+                await conn.StartAsync();
+                await Task.Delay(500);
+                await conn.StopAsync();
+                await Task.Delay(500);
+            }
+        }
+    }
+}
